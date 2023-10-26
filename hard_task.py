e58982f06f2fa77d125ca965f5185a3fd2e56e5f
marcus <marcusplay@mail.ru>
2023-10-27 01:13:59 +0300

    modified hard task

--- hard_task.py
+++ hard_task.py
@@ -8,6 +8,18 @@ if __name__=="__main__":
     word_2 = input()
     word_3 = input()
 
+    words_list = []
     for i in word_1:
-        if i in word_2 and i in word_3:
-            print(i)
+        if i not in word_2 and i not in word_3:
+            words_list.append(i)
+    for i in word_2:
+        if i not in word_1 and i not in word_3:
+            words_list.append(i)
+    for i in word_3:
+        if i not in word_2 and i not in word_1:
+            words_list.append(i)
+
+    print(set(words_list))
+            
+
+
